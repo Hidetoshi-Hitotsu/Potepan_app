66944b3c5f27f43c335057ce6d6f9d140fa01bd8
user <user@usernoMacBook-Pro.local>
2023-06-13 09:55:48 +0900

    add: 商品詳細ページのRspec導入
    商品詳細ページのリクエストスペックを導入しています。

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
-  describe "GET /show" do
+  describe "GET /potepan/products/:id" do
+    let!(:product) { create(:product) }
     it "returns http success" do
-      get "/potepan/products/show"
+      get potepan_product_path(product.id)
       expect(response).to have_http_status(:success)
     end
   end
-
 end
