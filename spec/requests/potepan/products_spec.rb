0b252623ef533cbf46346927c61255d614aa93a6
user <user@usernoMacBook-Pro.local>
2023-06-21 11:16:06 +0900

    環境変数の設定、rspecの修正

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe "Potepan::Products", type: :request do
   describe "GET /potepan/products/:id" do
     let!(:product) { create(:product) }
-    it "returns http success" do
-      get potepan_product_path(product.id)
-      expect(response).to have_http_status(:success)
-    end
-
     before do
       get potepan_product_path(product.id)
+    end
+
+    it "returns http success" do
+      expect(response).to have_http_status(:success)
     end
 
     it "商品名を取得できること" do
