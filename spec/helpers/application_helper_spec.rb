6ff034efd73d09aea221a33c282903b2bf54f938
user <user@usernoMacBook-Pro.local>
2023-06-27 22:09:40 +0900

    helperのrspecを追加

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe "pagetitle helper のテスト" do
+    context "pagetitleがnilである" do
+      it "BIGBAG Storeが表示される" do
+        expect(pagetitle('')).to eq('BIGBAG Store')
+      end
+    end
+
+    context "pagetitleがnilでない" do
+      it "pagetitleとBIGBAG Storeが表示される" do
+        expect(pagetitle('PageTitle')).to eq('PageTitle - BIGBAG Store')
+      end
+    end
+  end
+end
