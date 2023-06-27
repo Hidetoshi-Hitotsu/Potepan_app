8a481c9d01c360991dcf621be3e2f8bde214017c
user <user@usernoMacBook-Pro.local>
2023-06-27 23:33:41 +0900

    helperスペックの修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe "#page_title" do
     context "pagetitleがnilである" do
       it "BIGBAG Storeが表示される" do
-        expect(pagetitle('')).to eq('BIGBAG Store')
+        expect(page_title('')).to eq('BIGBAG Store')
       end
     end
 
     context "pagetitleがnilでない" do
       it "pagetitleとBIGBAG Storeが表示される" do
-        expect(pagetitle('PageTitle')).to eq('PageTitle - BIGBAG Store')
+        expect(page_title('PageTitle')).to eq('PageTitle - BIGBAG Store')
       end
     end
   end
