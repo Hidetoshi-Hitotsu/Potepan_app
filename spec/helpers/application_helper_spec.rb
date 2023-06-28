69db08e7f21e6c079c44bbcd260db5e3105c0ecf
user <user@usernoMacBook-Pro.local>
2023-06-28 10:21:54 +0900

    showページとパーシャルの修正、不要なコメントの削除

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe "#page_title" do
-    context "pagetitleがnilである" do
+    context "引数がnilである" do
       it "BIGBAG Storeが表示される" do
         expect(page_title('')).to eq('BIGBAG Store')
       end
     end
 
-    context "pagetitleがnilでない" do
+    context "引数がnilでない" do
       it "pagetitleとBIGBAG Storeが表示される" do
         expect(page_title('PageTitle')).to eq('PageTitle - BIGBAG Store')
       end
