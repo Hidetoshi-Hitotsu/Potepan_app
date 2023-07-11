20870de5fdcac555102a9cf150f1faa52261a631
user <user@usernoMacBook-Pro.local>
2023-07-11 12:16:29 +0900

    display_priceにおけるN＋1問題の解消

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,5 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.preload(:products).find(params[:id])
     @taxonomies = Spree::Taxonomy.all.includes(:root)
+    @products = @taxon.all_products.includes(master: [:default_price, images: { attachment_attachment: :blob } ])
   end
 end
